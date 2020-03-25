c2989f95d6690247c412503777cd1ba13cffaf1d
Mahmoud MAZIAD <maziad.mahmoud@gmail.com>
2020-03-25 21:56:36 +0000

    add requirements and some code

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 import tensorflow as tf
+# Download and import the MIT 6.S191 package
+#!pip install mitdeeplearning
+#!pip install ipython
 
+import mitdeeplearning as mdl
+import numpy as np
+import matplotlib.pyplot as plt
+
+class OurDenseLayer(tf.keras.layers.Layer):
+  def __init__(self, n_output_nodes):
+    super(OurDenseLayer, self).__init__()
+    self.n_output_nodes = n_output_nodes
+
+  def build(self, input_shape):
+    d = int(input_shape[-1])
+    # Define and initialize parameters: a weight matrix W and bias b
+    # Note that parameter initialization is random!
+    self.W = self.add_weight("weight", shape=[d, self.n_output_nodes]) # note the dimensionality
+    self.b = self.add_weight("bias", shape=[1, self.n_output_nodes]) # note the dimensionality
+
+  def call(self, x):
+    '''TODO: define the operation for z (hint: use tf.matmul)'''
+    z = tf.matmul(x,self.W) + self.b
+    '''TODO: define the operation for out (hint: use tf.sigmoid)'''
+    y = tf.sigmoid(z)
+    return y
+
+tf.random.set_seed(1)
+layer = OurDenseLayer(3)
+layer.build((1,2))
+x_input = tf.constant([[1,2.]], shape=(1,2))
+y = layer.call(x_input)
+
+# test the output!
+print(y.numpy())
+#mdl.lab1.test_custom_dense_layer_output(y)
